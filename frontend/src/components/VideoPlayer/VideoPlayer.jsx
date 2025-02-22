09850fb6f39a8018180978278577722f78c75234
Vishnu Menon <menonvishnu26@gmail.com>
2025-02-22 19:29:13 +0530

    Added Ad Overlay

--- frontend/src/components/VideoPlayer/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,20 +1,50 @@
 import "./VideoPlayer.css";
 import Video from "../../Assets/video-1.mp4";
+import { useState, useRef, useEffect } from "react";
 
-function VideoPlayer() {
+function VideoPlayer(props) {
+	const [ad, setAd] = useState(props.adsData[0]);
+	const videoRef = useRef(null); // Reference to the video element
+
+	useEffect(() => {
+		const adInterval = setInterval(() => {
+			setAd(props.adsData[      Math.floor( (Math.random() * 100) % props.adsData.length    )]);
+		}, 10000); // Change ad every 10 seconds
+
+		return () => clearInterval(adInterval);
+	}, []);
+
+	const handleAdClick = (event) => {
+		// event.preventDefault();
+		if (videoRef.current) {
+			const timestamp = videoRef.current.currentTime;
+			console.log("Ad clicked at timestamp:", timestamp);
+		}
+	};
+
 	return (
 		<div className="container">
-			<video className="video" controls>
+			<video ref={videoRef} className="video" controls>
 				<source src={Video} type="video/mp4" />
 				Your Browser does not support this video
 			</video>
 
-            <div className="ad-overlay">
-                <h2>This is ad display</h2>
-            </div>
+			<a
+				className="ad-overlay"
+				onClick={handleAdClick}
+				href={ad.target_url}
+				target="_blank"
+                rel="noreferrer"
+                >
+				<img src={ad.image_url} alt="" />
+			</a>
 		</div>
 	);
 }
 
 export default VideoPlayer;
 
+//next steps:
+/*
+    how to change positions randomly
+*/
