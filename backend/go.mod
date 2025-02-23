f3663f5e336c2425e7e44270aa38267b36f3ebde
Vishnu Menon <menonvishnu26@gmail.com>
2025-02-23 18:13:07 +0530

    added .env variables

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module github.com/MenonVishnu/Video-Ad-Player/backend
 
 go 1.22.2
 
-require github.com/mattn/go-sqlite3 v1.14.24
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/mattn/go-sqlite3 v1.14.24
+)
--- backend/main.go
+++ backend/main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/MenonVishnu/Video-Ad-Player/backend/controllers"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Could not find .env file")
+	}
+
 	http.HandleFunc("/api/v1/ads", controllers.GetAds)
 	http.HandleFunc("/api/v1/ads/click", controllers.LogClick)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(os.Getenv("PORT"), nil))
 }
